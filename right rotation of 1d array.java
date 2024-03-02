cdb81bfd7a5d11c447dd46dd7432d8099c8cc7ee
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-03-02 22:04:54 +0530

    Create right rotation of 1d array.java

---
+++ right rotation of 1d array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array size: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the how rotation in left: ");
+        int k = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        
+        k = k % n;
+        for (int j = 0; j < k; j++) {
+            int temp = arr[n-1];
+            for (int i = n-2; i >=0; i--) {
+                arr[i+1]= arr[i];
+            }
+            arr[0] = temp;
+        }
+        
+       System.out.print(Arrays.toString(arr));
+    }
+}
