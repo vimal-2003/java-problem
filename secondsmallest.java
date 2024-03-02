1444a6ff26e68bdd0479e77d06a020dce723730b
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-03-02 22:06:31 +0530

    Create secondsmallest.java

---
+++ secondsmallest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main
+{ 
+   static int secSmallest(int arr[], int n)
+   {
+        int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+
+        for (int i=0; i < n; i++){
+            if(arr[i] < first)
+            { 
+             second = first; 
+             first = arr[i]; 
+            
+            } 
+            else if(second>arr[i])
+           second = arr[i];
+        }
+
+        return second;
+
+   }
+   public static void main(String args[])
+   {
+
+      int arr[] = {12, 13, 1, 10, 34, 10};
+      int n = arr.length;
+      System.out.print(secSmallest(arr, n)); 
+   }
+}
