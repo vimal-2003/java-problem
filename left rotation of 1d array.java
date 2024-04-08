4adbcef0a469b2c2fc5f5037652465f1d4ec0c52
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-04-08 16:05:16 +0530

    Update left rotation of 1d array.java

--- left rotation of 1d array.java
+++ left rotation of 1d array.java
@@ -12,9 +12,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        
-        
-        k = k % n;
+         k = k % n;
         for (int j = 0; j < k; j++) {
             int temp = arr[0];
             for (int i = 1; i < n; i++) {
