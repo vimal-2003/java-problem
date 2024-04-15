22f5f785a2414ec8bed61f9681ca5f818bbf1641
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-04-15 20:40:54 +0530

    Update Happynumbers.java

--- Happynumbers.java
+++ Happynumbers.java
@@ -11,7 +11,6 @@ public class HappyNumber
         }  
         return sum;  
     }  
-      
     public static void main(String[] args) {  
         Scanner sc= new Scanner(System.in);
         int num =sc.nextInt() ;  
