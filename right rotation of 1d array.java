9fd4161ab76756e22f744d7f5869e2dddfb72b13
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-04-28 22:35:27 +0530

    Update right rotation of 1d array.java

--- right rotation of 1d array.java
+++ right rotation of 1d array.java
@@ -12,6 +12,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        
         k = k % n;
         for (int j = 0; j < k; j++) {
             int temp = arr[n-1];
